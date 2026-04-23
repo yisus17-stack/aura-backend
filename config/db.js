488a44581e20ad3cf446103c346a556f6d04878f
Yisus <esquivelyisus17@gmail.com>
2026-04-22 21:05:13 -0600

    fix env

--- config/db.js
+++ config/db.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('🟢 Mongo conectado');
   } catch (error) {
     console.log('🔴 Error Mongo:', error);
