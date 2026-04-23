f5bdc7cce05d8e71c7b4385893095b85671574df
Yisus <esquivelyisus17@gmail.com>
2026-04-22 20:46:47 -0600

    fix mongo atlas connection

--- config/db.js
+++ config/db.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/Aura');
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
     console.log('🟢 Mongo conectado');
   } catch (error) {
     console.log('🔴 Error Mongo:', error);
