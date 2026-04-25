b5e0efd710b48a1a7f22d45a132f1ddbdcd5801e
Yisus <esquivelyisus17@gmail.com>
2026-04-24 21:14:34 -0600

    agregacion env

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log('🟢 Mongo conectado');
   } catch (error) {
     console.log('🔴 Error Mongo:', error);
